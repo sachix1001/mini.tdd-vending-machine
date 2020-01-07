ae9f4472ab665f1a0dca228580b6d9ce0af94fc7
harunamarun <harunamarun@gmail.com>
2020-01-07 17:54:31 +0900

    implement VendingMachine

--- VendingMachine.js
+++ VendingMachine.js
@@ -4,3 +4,66 @@
   What is that? Well, glad you asked.
   Read about it here: https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 */
+
+class VendingMachine {
+  constructor() {
+    this.till = {
+      10: 0,
+      50: 0,
+      100: 0,
+      500: 0,
+    };
+    this.balance = 0;
+    this.selectedRow;
+    this.selectedColumn;
+    const juice = { name: `Apple Juice`, price: 350, count: 5 };
+    const coffee = { name: "Tully's", price: 250, count: 7 };
+    const tea = { name: `Grean tee`, price: 150, count: 2 };
+    const banana = { name: "Banana", price: 250, count: 4 };
+    this.inventory = [
+      [juice, coffee, tea, banana],
+      [juice, coffee, tea, banana],
+      [juice, coffee, tea, banana],
+      [juice, coffee, tea, banana],
+    ];
+  }
+  insertCoin(num) {
+    this.balance += num;
+    return this.till[num]++;
+  }
+  selectButton(btn) {
+    if (typeof btn === "string") {
+      if (this.selectedRow === undefined) {
+        const rowNum = {
+          A: 1,
+          B: 2,
+          C: 3,
+          D: 4,
+        };
+        this.selectedRow = rowNum[btn];
+        console.log("row: ", this.selectedRow);
+      }
+    } else if (typeof btn === "number") {
+      if (this.selectedColumn === undefined) {
+        this.selectedColumn = btn;
+      }
+      const item = this.inventory[this.selectedRow - 1][
+        this.selectedColumn - 1
+      ];
+      if (this.balance > item.price) {
+        item.count--;
+        console.log("item", item);
+        console.log(`row: ${this.selectedRow}
+          column: ${this.selectedColumn}
+          Here is your ${item.name}`);
+      }
+    }
+  }
+
+  changeReturn() {
+    this.balance = 0;
+    console.log("till: ", this.till);
+  }
+}
+
+module.exports = { VendingMachine };
--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const VendingMachine = require("../VendingMachine");
+const { VendingMachine } = require("../VendingMachine");
 const { expect } = require("chai");
 
 describe("vending machine", () => {
@@ -17,5 +17,48 @@ describe("vending machine", () => {
       500: 1,
     });
     expect(machine.balance).to.equal(500); // Use an ES6 getter
+  });
+  it("should store selected row", () => {
+    const machine = new VendingMachine();
+
+    machine.selectButton("A");
+
+    expect(machine.selectedRow).to.equal(1);
+  });
+
+  it("should store selected column", () => {
+    const machine = new VendingMachine();
+
+    machine.selectButton("A");
+    machine.selectButton(2);
+
+    expect(machine.selectedColumn).to.equal(2);
+  });
+
+  it("should only store a row if no column is selected", () => {
+    const machine = new VendingMachine();
+
+    machine.selectButton("A");
+    machine.selectButton(3);
+    machine.selectButton(1);
+
+    expect(machine.selectedColumn).to.equal(3);
+  });
+
+  it("should reset balance and logs coins", () => {
+    const machine = new VendingMachine();
+
+    machine.changeReturn();
+    expect(machine.balance).to.equal(0);
+  });
+
+  it("should decrease the selected item's count by 1", () => {
+    const machine = new VendingMachine();
+
+    machine.insertCoin(500);
+    machine.selectButton("A");
+    machine.selectButton(1);
+    const item = machine.inventory[0][0].count;
+    expect(item).to.equal(4);
   });
 });
